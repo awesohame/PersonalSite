f08c73880bf24157338cd5ab4e668786a4f28ce1
Awesohame <82087247+awesohame@users.noreply.github.com>
2023-10-21 23:29:22 +0530

    react port

---
+++ src/Navbar.js
@@ -0,0 +1,27 @@
+import './index.css';
+
+function Navbar() {
+  return (
+    <div className="header">
+      <nav className="navbar">
+        {/* <a href="#" className="pfpcontainer"><img src="/images/pfp.png" alt="SA" className="navimg pfp"></a>  */}
+        <a href="https://instagram.com/awesohame" class="navtext" id="initials">SA</a>
+        <ul id="navlist">
+          <li className="navicon"><a href="https://google.com" className="navElementR navtext">Resume</a></li>
+          <li className="navicon"><a href="https://github.com/awesohame" target="_blank" rel="noreferrer" className="navElement">
+            {/* <img src="images/github.png" alt="github" class="navimg"> */}
+            <i className="fa-brands fa-github"></i></a>
+          </li>
+          <li className="navicon"><a href="https://www.linkedin.com/in/sohamaversekar/" target="_blank" rel="noreferrer"
+            className="navElement">
+            <i className="fa-brands fa-linkedin"></i>
+            {/* <img src="images/linkedin.png" alt="linkedin" className="navimg">  */}
+          </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
